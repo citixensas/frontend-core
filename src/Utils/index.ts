991c6a5c6d4e72185d90110c6b8d2179bbe4d9f3
ramses manosalva <ramses.manosalva@gmail.com>
2020-03-03 17:37:56 -0500

    fix export utils

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,10 +1,24 @@
-export * from './LocalCurrencyConversion'
-export * from './GetVisibilityNames'
-export * from './ValidateTelephone'
-export * from './WhatsappFormater'
-export * from './SearchByFields'
-export * from './CompressFile'
-export * from './Notification'
-export * from './FindAction'
-export * from './General'
-export * from './Chat'
+import * as LocalCurrencyConversion from './LocalCurrencyConversion'
+import * as GetVisibilityNames from './GetVisibilityNames'
+import * as ValidateTelephone from './ValidateTelephone'
+import * as WhatsappFormater from './WhatsappFormater'
+import * as SearchByFields from './SearchByFields'
+import * as CompressFile from './CompressFile'
+import * as Notification from './Notification'
+import * as FindAction from './FindAction'
+import * as General from './General'
+import * as Chat from './Chat'
+
+
+export default {
+    LocalCurrencyConversion,
+    GetVisibilityNames,
+    ValidateTelephone,
+    WhatsappFormater,
+    SearchByFields,
+    CompressFile,
+    Notification,
+    FindAction,
+    General,
+    Chat,
+}
